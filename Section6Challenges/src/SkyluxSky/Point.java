53e112f2e83f6066ba1839b7a68096a844d9b3ea
SkyluxSky <skyluxsky@gmail.com>
2021-04-11 00:33:34 -0400

    Point Exercise -  Completed

---
+++ Section6Challenges/src/SkyluxSky/Point.java
@@ -0,0 +1,53 @@
+package SkyluxSky;
+
+public class Point {
+
+    private int x;
+    private int y;
+    
+//    The class needs to have two constructors. The first constructor does not have any parameters (no-arg constructor).
+//    The second constructor has parameters x and y of type int and it needs to initialize the fields.
+
+    public Point(){
+
+    }
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    //=================================================================================
+
+    public double distance(){
+
+        return Math.sqrt( (getX() - 0.0) * ( getX() - 0)  + (getY() - 0) * (getY() - 0));
+    }
+
+    public double distance(int x, int y){
+
+        return Math.sqrt( (getX() - x) * ( getX() - x)  + (getY() - y ) * (getY() - y));
+    }
+
+    public double distance(Point xy){
+
+        return Math.sqrt( (getX() - xy.getX()) * ( getX() - xy.getX())  + (getY() - xy.getY() ) * (getY() - xy.getY()));
+    }
+
+}
