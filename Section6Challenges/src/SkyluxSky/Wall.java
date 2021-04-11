a2a4616ce12653a42956814dce2c5dd5becb6d0a
SkyluxSky <skyluxsky@gmail.com>
2021-04-11 00:28:00 -0400

    Wall Exercise - Completed - Used Ternary Operators

---
+++ Section6Challenges/src/SkyluxSky/Wall.java
@@ -0,0 +1,39 @@
+package SkyluxSky;
+
+public class Wall {
+    //Field
+    private double width;
+    private double height;
+
+    //Constructor
+    public Wall(double width, double height) {
+        this.width = width < 0 ? 0 : width;
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public Wall() {
+        this(0, 0);
+    }
+
+    //Method
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public void setWidth(double width) {
+        this.width = width < 0 ? 0 : width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height < 0 ? 0 : height;
+    }
+
+}
